f8459e4774610710ae01d92ba1e3697f53a8f6b3
Chad Estioco <chadestioco@gmail.com>
2016-12-06 03:53:53 +0800

    Add a bit more (self-)documentation becuase I feel that this will still be usable in the future.

--- scripts/utf_converter.py
+++ scripts/utf_converter.py
@@ -5,9 +5,18 @@ import sys
 
 """
 Converts a UTF-16 file to UTF-8.
+
+TODO Maybe make this invokable from anywhere?
 """
 
 if __name__ == "__main__":
-    with codecs.open(sys.argv[1], "rb", "utf-16") as utf16_file:
-        with codecs.open(sys.argv[2], "wb", "utf-8") as utf8_file:
+    if len(sys.argv) != 3:
+        print("Usage: python3 %s utf16_source utf8_target" % sys.argv[0])
+        exit(1)
+
+    source = sys.argv[1]
+    dest = sys.argv[2]
+
+    with codecs.open(source, "rb", "utf-16") as utf16_file:
+        with codecs.open(dest, "wb", "utf-8") as utf8_file:
             utf8_file.write(utf16_file.read())
