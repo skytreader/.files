d774796e3eae0e080c2b4ac1aa7aa8553a161af7
Chad Estioco <chadestioco@gmail.com>
2018-04-04 04:22:52 +0800

    Add import_merger...we might find use for you again someday.

---
+++ scripts/import_merger.py
@@ -0,0 +1,8 @@
+def normalize_imports(i):
+    sans_breaks = " ".join([l.strip() for l in i.split("\n")])
+    return set(sans_breaks.split(", "))
+
+def merge_imports(i1, i2):
+    ni1 = normalize_imports(i1)
+    ni2 = normalize_imports(i2)
+    return ", ".join(ni1 | ni2)
