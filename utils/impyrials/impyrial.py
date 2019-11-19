a5d4883cd02100b879e87ea296320b333c36fbcd
Chad Estioco <chadestioco@gmail.com>
2019-11-19 17:06:08 +0100

    Ensure there is only one JAR file ever in the target directory.

--- utils/impyrials/impyrial.py
+++ utils/impyrials/impyrial.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+import glob
 import logging
 import os
 import shutil
@@ -37,11 +38,27 @@ class CopyFileHandler(FileSystemEventHandler):
 
     def __is_jar_event(self, event):
         return not event.is_directory and event.src_path.endswith(".jar")
+    
+    def __move_other_files(self, source_filename, existing_files):
+        sourcefname_split = source_filename.split(os.path.sep)
+        sourcef_plain_fname = sourcefname_split[-1]
+
+        for filename in existing_files:
+            # Normalize the filenames
+            pathsep_split = filename.split(os.path.sep)
+            existingf_plain_fname = pathsep_split[-1]
+
+            if existingf_plain_fname != sourcef_plain_fname:
+                logging.info("SOFT DELETE: Moving %s to /tmp/%s." % (filename, existingf_plain_fname))
+                shutil.move(filename, "/tmp/%s" % existingf_plain_fname)
 
     def on_created(self, event):
         super().on_created(event)
         if self.__is_jar_event(event):
             logging.info("JAR created: %s" % event.src_path)
+            existing_files = glob.glob("%s/*.jar" % self.copyto)
+            if existing_files:
+                self.__move_other_files(event.src_path, existing_files)
             shutil.copy(event.src_path, self.copyto)
 
     def on_modified(self, event):
