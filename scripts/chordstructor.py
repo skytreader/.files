485b03e332384787f51bf8ba165d6d6d372336bb
Chad Estioco <chadestioco@gmail.com>
2020-11-27 16:02:39 +0100

    Fix typing and add space between notetations.

--- scripts/chordstructor.py
+++ scripts/chordstructor.py
@@ -12,7 +12,7 @@ And the input file/stream is just a series of zeitpunkten.
 from typing import List, Tuple
 import sys
 
-ZeitPunkten = Tuple[Tuple[Tuple[int, int]]]
+ZeitPunkten = Tuple[Tuple[Tuple[int, int], ...], ...]
 
 def measured_chordstructor(zeitpunkten: ZeitPunkten, measure: int) -> str:
     start = 0
@@ -20,22 +20,28 @@ def measured_chordstructor(zeitpunkten: ZeitPunkten, measure: int) -> str:
     return "\n".join(tabs)
 
 def chordstructor(zeitpunkten: ZeitPunkten) -> str:
+    def note_width(fret_no: int) -> int:
+        return 3 if fret_no >= 10 else 2
+
     lines: List[List[str]] = [['|'] for _ in range(6)]
     strings = set(range(6))
 
     for zp in zeitpunkten:
         zp_used = set()
+        max_width_added = 0
 
         for fingerpos in zp:
             str_in = fingerpos[0] - 1;
             zp_used.add(str_in)
             
-            lines[str_in] = str(fingerpos[1])
+            lines[str_in].append(str(fingerpos[1]))
+            lines[str_in].append('-')
+            max_width_added = max(max_width_added, note_width(fingerpos[1]))
 
         zp_blanks = strings - zp_used
 
         for blank in zp_blanks:
-            lines[blank] = '-'
+            lines[blank].append('-' * max_width_added)
 
     return "|\n".join(["".join(line) for line in lines])
 
@@ -51,4 +57,4 @@ if __name__ == "__main__":
             fret_nums = point[1::2]
             zeitpunkten.append(tuple(zip(string_nums, fret_nums)))
 
-        print(measured_chordstructor(tuple(zeitpunkten)))
+        print(measured_chordstructor(tuple(zeitpunkten), 30))
